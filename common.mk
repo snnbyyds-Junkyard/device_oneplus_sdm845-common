3185fc05fd25be9d1327d223acbd5658cb1fb092
Erfan Abdi <erfangplus@gmail.com>
2022-09-07 17:00:36 +0530

    sdm845-common: switch to move to in-tree HALS
    Change-Id: Iad2d277d6df1edf928cf3bfda962b62c2341b423

--- common.mk
+++ common.mk
@@ -378,6 +378,9 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/oneplus \
     vendor/qcom/opensource/usb/etc
 
+QCOM_SOONG_NAMESPACE := \
+    $(LOCAL_PATH)/oss-hals
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
